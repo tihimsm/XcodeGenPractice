8f10ae4b01a4ef3f9ffdf1b36fc6d512bac0ca50
tihimsm <tihimsm@gmail.com>
2019-11-22 12:54:30 +0900

    Initial Commit

---
+++ XcodeGenPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenPractice
+//
+//  Created by t-mishima on 2019/11/22.
+//  Copyright © 2019 tihimsm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
